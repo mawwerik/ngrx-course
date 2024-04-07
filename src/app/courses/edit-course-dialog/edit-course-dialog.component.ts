49eb7810dd76cecd60118d67f48cc8a0d48e20dd
Zoltán Izsó <zoltan.izso@hu.ibm.com>
2024-04-07 11:59:03 +0200

    Course add with ngrx-data

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -65,6 +65,13 @@ export class EditCourseDialogComponent {
     if (this.mode === 'update') {
       this.coursesService.update(course);
       this.dialogRef.close();
+    } else if (this.mode === 'create') {
+      this.coursesService.add(course)
+        .subscribe(
+          _newCourse => {
+            this.dialogRef.close();
+          }
+        );
     }
   }
 
